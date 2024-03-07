547188a3ed09bbc53b6c1a46b3f097038d06c0a6
HZC-1998 <110812756+HZC-1998@users.noreply.github.com>
2024-03-07 23:39:35 +0800

    Add files via upload

---
+++ ARM.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+class ARM(nn.Module):
+
+    def __init__(self):
+        super(ARM, self).__init__()
+        self.Conv1 = nn.Conv2d(4, 4, 3, 1, 1)
+        self.Conv2 = nn.Conv2d(4, 4, 3, 1, 1)
+        self.Conv3 = nn.Conv2d(4, 4, 3, 1, 1)
+        self.Conv4 = nn.Conv2d(4, 4, 3, 1, 1)
+        self.Conv5 = nn.Conv2d(4, 4, 3, 1, 1)
+        self.Relu = nn.ReLU()
+        self.Sig = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.Conv1(x)
+        out = self.Conv2(self.Relu(out))
+        out = self.Conv3(self.Relu(out))
+
+        Z = self.Conv5(self.Relu(out))
+        M = self.Sig(self.Conv4(self.Relu(out)))
+
+        out = M*out + (1-M)*Z
+
+        return out
